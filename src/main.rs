e3b70edad451300bd444f69dc549493723e0eb82
Taylor <96550502+taylors42@users.noreply.github.com>
2024-02-16 13:07:02 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::io; // <- importando biblioteca de io de dentro de std
+// use std::io; // <- importando biblioteca de io de dentro de std
 fn main() {
-    let name &str = "taylor"; // <- variavel não mutavel
-    !let mut name1 &str = "taylor mutavel"; // <- variavel mutavel
-    let mut vazio = String::new() ; // <- cria uma string vazia e new é um metodo da classe string
+    let name: &str = "taylor";
+    // let mut name1 &str = "taylor mutavel";
+    // let mut vazio = String::new() ;
     println!("Thats my first script in RUST, and I'm {}", name);
-    io::stdin().read_line(&mut vazio)
-    //stdin is a method inside the io thats read a line
-    //(&mut) indica que estamos referenciando um valor mutavel isso serve para não criar copias na memoria
-    // que o read_line pode modificar o conteudo dentro do vazio
-    //vazio é onde será armazenado o valor que o usuario digitar
-        .expect("Falha ao ler entrada"); // <- caso de erro, retornar str
+    // io::stdin().read_line(&mut vazio).expect("Falha ao ler entrada");
 }
