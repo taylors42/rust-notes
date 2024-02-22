35e13b6564634a4edaa63148c515ad61b3dc069b
Taylor <96550502+taylors42@users.noreply.github.com>
2024-02-22 16:32:54 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 use rand::Rng;
 fn main() {
-    
+    sum_all_digits();
 }
 
 fn hello_world(){
@@ -26,7 +26,7 @@ fn flux_control(num1: i32, num2: i32){
     }
 }
 
-fn convert_to_int(data_input: &String) -> i32{
+fn convert_to_int(data_input: &String) -> i32 {
     let x: i32 = data_input.trim().parse::<i32>().unwrap(); //unwrap will panic if there is an error and get the value
     x // return the value
 }
@@ -48,4 +48,19 @@ fn flux_control_by_user(){
     else {
         println!("{num1} is equal to {num2}");
     }
+}
+
+fn sum_all_digits(){
+    let mut total: i32 = 0;
+    let mut initial_value = String::new();
+    io::stdin().read_line(&mut initial_value)
+    .expect("err");
+    let mut value: i32 = convert_to_int(&initial_value);
+
+    while value != 0 {
+        let mut r: i32 = value % 10;
+        total = total + r;
+        value = value / 10;
+    }
+    println!("{total}");
 }
