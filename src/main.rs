42d932094a9e550bd52eb3986b434ebadb602065
Taylor <96550502+taylors42@users.noreply.github.com>
2024-02-22 09:04:45 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::io;
 use rand::Rng;
 fn main() {
-    hello_world();
-    say_user_input();
+    
 }
 
 fn hello_world(){
@@ -13,5 +12,40 @@ fn say_user_input(){
     let mut user_input = String::new();
     io::stdin().read_line(&mut user_input).expect("err");
     println!("{user_input}");
+}
 
+fn flux_control(num1: i32, num2: i32){
+    if num1 > num2 {
+        println!("{num1} is greater than {num2}");
+    }
+    else if num1 < num2 {
+        println!("{num1} is less than {num2}");
+    }
+    else {
+        println!("{num1} is equal to {num2}");
+    }
+}
+
+fn convert_to_int(data_input: &String) -> i32{
+    let x: i32 = data_input.trim().parse::<i32>().unwrap(); //unwrap will panic if there is an error and get the value
+    x // return the value
+}
+
+fn flux_control_by_user(){
+    let mut num1: String = String::new();
+    let mut num2: String = String::new();
+    io::stdin().read_line(&mut num1)
+    .expect("fail");
+    io::stdin().read_line(&mut num2)
+    .expect("fail");
+
+    if convert_to_int(&num1) > convert_to_int(&num2){
+        println!("{num1} is greater than {num2}");
+    }
+    else if convert_to_int(&num1) < convert_to_int(&num2) {
+        println!("{num1} is less than {num2}");
+    }
+    else {
+        println!("{num1} is equal to {num2}");
+    }
 }
