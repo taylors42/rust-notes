d3a4622b0e823eb9091d14e6e98e6500692cc8ca
Taylor <96550502+taylors42@users.noreply.github.com>
2024-03-10 21:17:48 -0300

    added a bubble_sort

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,49 @@
 use std::io;
 use rand::Rng;
 fn main() {
-    mdc();
+    let mut array: [i32; 100] = [
+    58, 20, 77, 33, 10, 99, 50, 30, 2, 75,
+    88, 60, 37, 89, 9, 70, 36, 14, 5, 34,
+    38, 92, 40, 64, 31, 21, 79, 78, 13, 83,
+    25, 71, 41, 15, 93, 18, 46, 44, 6, 28,
+    51, 22, 80, 1, 7, 39, 49, 17, 90, 67,
+    26, 84, 3, 45, 72, 11, 96, 29, 65, 32,
+    94, 55, 76, 4, 85, 42, 66, 8, 23, 52,
+    12, 97, 73, 35, 86, 43, 68, 16, 24, 53,
+    19, 98, 74, 47, 87, 56, 69, 48, 27, 54,
+    81, 100, 82, 57, 91, 62, 95, 63, 61, 59
+];
+    println!("Before -> {:?}", array);
+    bubble_sort(&mut array);
+    println!("Late -> {:?}", array)
+}
+fn bubble_sort(array: &mut [i32]){
+    let is_ascending: bool = array.windows(2).all(|w| w[0] <= w[1]);
+    if is_ascending {
+        return
+    } else {
+        for i in 1..array.len(){
+            if array[i - 1] >= array[i]{
+                array.swap(i - 1, i);
+            }
+        }
+        bubble_sort(array);
+    }
+}
+
+fn greater(num1: i32, num2:i32) -> i32 {
+    if num1 > num2 {
+        num1
+    }
+    else if num1 < num2 {
+        num2
+    }
+    else {
+        num2
+    }
+}
+fn double(num: i32) -> i32 {
+   2*num
 }
 fn mdc(){
     let mut user_input = String::new();
@@ -41,7 +83,7 @@ fn mdc(){
     multiplicador *= divisores[i];
     }
     println!("Maximo divisor comum = {multiplicador}");
-    }
+}
 
 fn media_aluno(){
     let mut user_input = String::new();
